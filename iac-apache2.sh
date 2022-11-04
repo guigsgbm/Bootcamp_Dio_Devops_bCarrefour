2e2be51a7a3313da6a3164c58cc5d3454642081b
Guilherme Braga Magalhães <guilherme.magalhaes@mkth.com.br>
2022-11-04 12:21:29 -0300

    1.0

--- iac-apache2.sh
+++ iac-apache2.sh
@@ -2,19 +2,20 @@
 
 echo "Baixando dependências no servidor"
 
-apt update -y
-apt install apache2 -y
-apt install unzip -y
+apt-get update -y
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
 
 echo "Baixando arquivos do código fonte"
 
-mkdir /temp & cd /temp
+cd /var/tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 
 echo "Extraindo e direcionando arquivos"
 
 unzip main.zip
-rm -r main.zip
 cd linux-site-dio-main
-cp -r . /var/www
-rm -r linux-site-dio-main
+cp -r * /var/www/html
+
+rm -r /var/tmp
